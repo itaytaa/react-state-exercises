a4981fef57b5e9a42e419571892697d47f5a0770
itay taasa <itaytaasa@gmail.com>
2021-01-04 15:10:54 +0200

    car+counter

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -17,15 +17,25 @@ class Counter extends Component {
 	}
 
 	increase() {
-		this.setState({
-			num: this.state.num + 1
-		});
+		if (this.state.num !== 20) {
+			this.setState({
+				num: this.state.num + 1
+			});
+		}
 	}
 
 	decrease() {
+		if (this.state.num !== 0) {
+			this.setState({
+				num: this.state.num - 1
+			});
+		}
+	}
+
+	increase10() {
 		this.setState({
-			num: this.state.num - 1
-		});
+			num: (this.state.num + 10 > 20) ? 20 : this.state.num + 10
+		})
 	}
 
 	render() {
@@ -34,6 +44,7 @@ class Counter extends Component {
 				<h1>Counter: </h1>
 				<button onClick={this.increase.bind(this)}>Increase</button>
 				<button onClick={this.decrease.bind(this)}>Decrease</button>
+				<button onClick={this.increase10.bind(this)}>Increse by 10</button>
 				<span>{this.state.num}</span>
 			</div>
 		);
